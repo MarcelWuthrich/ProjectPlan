bff75d6e5af42d2e9c46d0691dbd1e667c9521f3
Marcel Wüthrich <marcel.wuthrich@wunet.ch>
2020-12-04 17:56:21 +0100

    Sauvegarde du jour

--- ProjectPlan/frmCustomers.vb
+++ ProjectPlan/frmCustomers.vb
@@ -1,3 +1,12 @@
 ﻿Public Class frmCustomers
+    Private Sub frmCustomers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
 
+        Catch ex As Exception
+
+        End Try
+        'TODO: This line of code loads data into the 'DsCustomers.vcustomers' table. You can move, or remove it, as needed.
+        Me.VcustomersTableAdapter.Fill(Me.DsCustomers.vcustomers)
+
+    End Sub
 End Class
